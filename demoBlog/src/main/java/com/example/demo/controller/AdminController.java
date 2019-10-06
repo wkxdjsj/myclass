a524681fc1cf46bd4f524ccb2a4eb53ee24fb08e
kaixiang <wkxdjsj@gmail.com>
2019-10-06 14:28:44 -0400

    redo Login page

--- demoBlog/src/main/java/com/example/demo/controller/AdminController.java
+++ demoBlog/src/main/java/com/example/demo/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+
 import com.example.demo.model.User;
+import com.example.demo.model.UserLoginInfo;
 import com.example.demo.service.UserLogin;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ public class AdminController {
 
     @Autowired
     private UserService userService;
+    private UserLogin userLogin;
 
     @GetMapping("/index")
     public String index() {
@@ -22,17 +25,21 @@ public class AdminController {
 
     }
 
-    @Autowired
-    private UserLogin userLogin;
     @PostMapping("/login")
-    public @ResponseBody
-    String addUser(@RequestParam String username,
+    public String userLogin(@RequestParam String username,
                    @RequestParam String password,
                    HttpSession session,
                    RedirectAttributes redirectAttributes) {
-        UserLogin user = userLogin.login(username, password);
+        UserLoginInfo user = userLogin.userLogin(username, password);
 
-        return "SUCCESS!";
+        if (user != null) {
+            user.setPassword(null);
+            session.setAttribute("user", user);
+            return "admin/index";
+        } else {
+            redirectAttributes.addFlashAttribute("message", "用户名或密码错误");
+            return "redirect:/admin";
+        }
     }
 //    @PostMapping
 //    public String postTest(){
--- demoBlog/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demoBlog/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,8 @@
 package com.example.demo;
 
 import com.example.demo.dao.UserDao;
-import com.example.demo.model.User;
+import com.example.demo.dao.UserLoginDao;
+import com.example.demo.model.UserLoginInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +14,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
 	@Autowired
 	private UserDao userDao;
+	private UserLoginDao userLoginDao;
 
 	@Test
 	public void contextLoads() {
-		User user = userDao.findByName("Jone");
-		System.out.println(user.toString());
+//		User user = userDao.findByName("Jone");
+//		System.out.println(user.toString());
+
+		UserLoginInfo userLoginInfo = userLoginDao.userLogin("user1", "password1");
+		System.out.println(userLoginInfo.toString());
 	}
 
 }
